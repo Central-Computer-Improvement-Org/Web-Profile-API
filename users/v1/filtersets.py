473a036ce907c461341a76eeef05099eeac757c8
Faiz Elfahad Kurniawan <74404911+siunlucky@users.noreply.github.com>
2024-05-03 21:07:38 +0700

    RBLD-68 | CRUD System - Implement CRUD | Role Member

--- users/v1/filtersets.py
+++ users/v1/filtersets.py
@@ -30,3 +30,31 @@ class UserFilterSet(django_filters.FilterSet):
         start_date = datetime.strptime(value, "%d-%m-%Y")
 
         return queryset.filter(**{f"year_community_enrolled": start_date.strftime('%Y-%m-%d')})
+    
+class RoleFilterSet(django_filters.FilterSet):
+    dateField = django_filters.CharFilter(method='filter_dateField')
+    startDate = django_filters.CharFilter(method='filter_startDate')
+    endDate = django_filters.CharFilter(method='filter_endDate')
+    name = django_filters.CharFilter(field_name='name', lookup_expr='icontains')
+
+    def filter_dateField(self, queryset, name, value):
+        if value == 'updatedAt':
+            self.dateField_value = "updated_at"
+        else:
+            self.dateField_value = "created_at"
+
+        return queryset
+
+    def filter_startDate(self, queryset, name, value):
+        try:
+            start_date = datetime.strptime(value, "%d-%m-%Y %H:%M")
+            return queryset.filter(**{f"{self.dateField_value}__gte": start_date.strftime('%Y-%m-%d %H:%M')})
+        except ValueError:
+            return queryset.none()
+
+    def filter_endDate(self, queryset, name, value):
+        try:
+            end_date = datetime.strptime(value, "%d-%m-%Y %H:%M")
+            return queryset.filter(**{f"{self.dateField_value}__lte": end_date.strftime('%Y-%m-%d %H:%M')})
+        except ValueError:
+            return queryset.none()
