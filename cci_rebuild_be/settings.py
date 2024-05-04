1a3015f3942c10c06593e2a1ee63e6bfaf45dd79
Faiz Elfahad Kurniawan <74404911+siunlucky@users.noreply.github.com>
2024-05-05 01:41:53 +0700

    Fix Cors

--- cci_rebuild_be/settings.py
+++ cci_rebuild_be/settings.py
@@ -62,6 +62,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'cci_rebuild_be.urls'
