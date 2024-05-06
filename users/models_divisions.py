72c7430d1be9ea28af92cff854cbda9e73744258
Rahman Hakim <rahmanhakim2435@gmail.com>
2024-05-06 18:29:37 +0700

    add logo to divisions

--- users/models_divisions.py
+++ users/models_divisions.py
@@ -6,6 +6,7 @@ class Division(models.Model):
     id = models.CharField(primary_key=True, max_length=255)
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
+    logo = models.ImageField(upload_to="uploads/divisions/logo/", null=True)
 
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(auto_now_add=True, null=True)
