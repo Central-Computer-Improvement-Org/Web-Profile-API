d576d26ca1746c09ed265033d0d713a6b10f9930
Rahman Hakim <rahmanhakim2435@gmail.com>
2024-05-07 18:46:37 +0700

    add public user

--- users/urls.py
+++ users/urls.py
@@ -2,21 +2,22 @@ from django.urls import path, include
 
 from rest_framework import routers
 
-from .v1.views import UserProfileViewSet, UserViewSet, CMSDivisionViewSet, PublicDivisionViewSet, CMSRoleViewSet, PublicRoleViewSet
+from .v1.views import UserProfileViewSet, UserViewSet, CMSDivisionViewSet, PublicDivisionViewSet, CMSRoleViewSet, \
+    PublicRoleViewSet, PublicUserViewSet
 
 urlpatterns_v1_cms = [
     path('', UserViewSet.as_view({
         'get': 'list',
         'patch': 'update',
         'delete': 'destroy',
-    }), name='users'),
-    path('roles/', CMSRoleViewSet.as_view({
+    }), name='cms_users'),
+    path('roles', CMSRoleViewSet.as_view({
         'get': 'list',
         'post': 'create',
         'patch': 'update',
         'delete': 'destroy',
-    }), name='users'),
-    path("divisions/", CMSDivisionViewSet.as_view({
+    }), name='cms_roles'),
+    path("divisions", CMSDivisionViewSet.as_view({
         'get': 'list',
         'delete': 'destroy',
         'post': 'create',
@@ -26,13 +27,16 @@ urlpatterns_v1_cms = [
 ]
 
 urlpatterns_v1_public = [
-    path('profile/', UserProfileViewSet.as_view({
+    path('', PublicUserViewSet.as_view({
+        'get': 'list',
+    }), name='users'),
+    path('profile', UserProfileViewSet.as_view({
         'get': 'retrieve',
     }), name='profile'),
-    path('divisions/', PublicDivisionViewSet.as_view({
+    path('divisions', PublicDivisionViewSet.as_view({
         'get': 'list',
     }), name='divisions'),
-    path('roles/', PublicRoleViewSet.as_view({
+    path('roles', PublicRoleViewSet.as_view({
         'get': 'list',
     }), name='roles'),
 ]
