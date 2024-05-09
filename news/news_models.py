bc24906fcee53adf3e3f60ab1d912fb7447574d8
Rahman Hakim <rahmanhakim2435@gmail.com>
2024-05-09 22:10:40 +0700

    RBLD-103 | finish | Change News response and add visitedCount to news

--- news/news_models.py
+++ news/news_models.py
@@ -7,7 +7,9 @@ class News(models.Model):
     id = models.CharField(max_length=255, primary_key=True)
     title = models.CharField(max_length=255)
     description = models.TextField()
-    media_uri = models.ImageField(upload_to="uploads/news/thumbnails/", validators=[validate_image_size])
+    media_uri = models.ImageField(upload_to="uploads/news/thumbnails/", validators=[validate_image_size], null=True)
+
+    visited_count = models.IntegerField(default=0)
 
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(auto_now_add=True, null=True)
