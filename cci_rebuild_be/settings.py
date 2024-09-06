80b0b70ffae0c7266d70b399e7731be8dafd9427
kevinirawand <kevinid07@gmail.com>
2024-09-07 05:34:33 +0700

    staging

--- cci_rebuild_be/settings.py
+++ cci_rebuild_be/settings.py
@@ -82,7 +82,8 @@ CORS_ALLOWED_ORIGINS = [
     'https://freedb.tech',
     'https://staging-web-profile.dns-dynamic.net',
     'http://103.187.147.80:3001',
-    'http://103.187.147.80'
+    'http://103.187.147.80',
+    'http://103.187.147.80:3000'
 ]
 
 CORS_ALLOW_HEADERS = (
